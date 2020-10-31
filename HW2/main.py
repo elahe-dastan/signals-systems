6883f26f1ec13a9c39c875b353198d5f9671ab32
elahe dastan <elahe.dstn@gmail.com>
2020-10-31 19:40:21 +0330

    part b

--- HW2/main.py
+++ HW2/main.py
@@ -23,16 +23,29 @@ def convolve(a, b):
     return ans
 
 
-t = np.arange(-10, 10, 0.1)
+# The second part of the question
+# a)
+# t = np.arange(-10, 10, 0.1)
+#
+# x = []
+# for i in t:
+#     x.append(1 / 2 * math.exp(-2 * i) * np.heaviside(i, 1))
+#
+# h = np.heaviside(t, 1) - np.heaviside(t - 5, 1)
+#
+# plt.plot(convolve(x, h))
+# plt.show()
 
-x = []
-for i in t:
-    x.append(1 / 2 * math.exp(-2 * i) * np.heaviside(i, 1))
-
-h = np.heaviside(t, 1) - np.heaviside(t - 5, 1)
-
-plt.plot(convolve(x, h))
-plt.show()
-
+# b)
+# n = np.arange(-5, 11, 1)
+#
+# x = []
+# for i in n:
+#     x.append(math.pow(1/3, -n[i]) * np.heaviside(-n[i] - 1, 1))
+#
+# h = np.heaviside(n - 1, 1)
+#
+# plt.plot(convolve(x, h))
+# plt.show()
 
 
