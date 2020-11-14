3c17ee0a2202e45cbff4a2302da95dc8492d2d65
elahe dastan <elahe.dstn@gmail.com>
2020-11-14 09:18:35 +0330

    bk

--- HW3/main.py
+++ HW3/main.py
@@ -2,21 +2,30 @@ import numpy as np
 import array as arr
 
 
-def ak(T, x, c):
+def coefficient(T, x, y, c):
     start = 0
     end = T
     step = 0.001
     t = np.arange(start, end, step)
     ans = arr.array('d', [])
-    for k in range(1, c+1):
+    for k in range(1, c + 1):
         w0 = 2 * np.pi / T
-        partial = x(t) * np.cos(k * w0 * t)
+        partial = x(t) * y(k * w0 * t)
         integral = np.sum(partial) * step
         ans.append((2 / T) * integral)
     return ans
 
+
+def ak(T, x, c):
+    return coefficient(T, x, np.cos, c)
+
+
+def bk(T, x, c):
+    return coefficient(T, x, np.sin, c)
+
+
 def x(input):
-    return input**2
+    return input ** 2
 
-print(ak(2,x,1))
 
+print(bk(2, x, 1))
