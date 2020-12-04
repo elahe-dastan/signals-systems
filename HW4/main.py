31c37228eb9c88dd9bbc26d6c1269efb7ea85444
elahe dastan <elahe.dstn@gmail.com>
2020-12-04 12:41:28 +0330

    plot

--- HW4/main.py
+++ HW4/main.py
@@ -1,9 +1,20 @@
 from scipy.io import wavfile
-import scipy
+from scipy.fft import fft
 from scipy import fftpack
 import numpy as np
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 
 # get the file sample rate and data
 s_rate, signal = wavfile.read('samples/sample_1.wav')
-print(s_rate, signal)
+
+# fast fourier transform
+FFT = abs(fft(signal))
+
+# define frequency vector
+freqs = fftpack.fftfreq(len(FFT), 1/s_rate)
+
+# plot
+plt.plot(freqs, FFT)
+plt.xlabel("Frequency (Hz)")
+plt.ylabel("Amplitude")
+plt.show()
